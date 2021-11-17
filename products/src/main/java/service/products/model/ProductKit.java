0e271d881b63a300e1bd59cd26056324f78dd47d
nzeyvan <nzeyvan@hotmail.fr>
2021-11-17 23:44:21 +0100

    products

---
+++ products/src/main/java/service/products/model/ProductKit.java
@@ -0,0 +1,58 @@
+package service.products.model;
+
+public class ProductKit {
+    
+    private int id;
+    private String nom;
+    private String description;
+    private int prix;
+    private String categorie;
+
+    public ProductKit(int id, String nom, String description, int prix, String categorie) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+        this.prix = prix;
+        this.categorie = categorie;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+}
