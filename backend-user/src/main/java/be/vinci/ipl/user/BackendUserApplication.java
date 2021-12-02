5ec3460306dd00369bde29a0c21b43027eb8fea2
Senhaji Obey <obey.senhaji@student.vinci.be>
2021-12-02 18:31:41 +0100

    fix Eureka server

--- backend-user/src/main/java/be/vinci/ipl/user/BackendUserApplication.java
+++ backend-user/src/main/java/be/vinci/ipl/user/BackendUserApplication.java
@@ -3,10 +3,8 @@ package be.vinci.ipl.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaServer
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
--- eureka/src/main/java/be/vinci/ipl/eureka/EurekaApplication.java
+++ eureka/src/main/java/be/vinci/ipl/eureka/EurekaApplication.java
@@ -5,17 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
-
-	@SpringBootApplication
-	@EnableEurekaServer
-	public static class RegistryApplication {
 		public static void main(String[] args) {
-			SpringApplication.run(RegistryApplication.class, args);
+			SpringApplication.run(EurekaApplication.class, args);
 		}
-	}
 }
